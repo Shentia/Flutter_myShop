b2036d809b956609221e5c2eba6c681aa1c0632a
Ahmadreza Shamimi <Ahmadreza.Shamimi@gmail.com>
2022-04-06 23:20:48 -0400

    Add to Firebase

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import './product.dart';
+import 'package:http/http.dart' as http;
 
 class Products with ChangeNotifier {
   List<Product> _items = [
@@ -65,17 +67,32 @@ class Products with ChangeNotifier {
   }
 
   void addProduct(Product product) {
+    final url =
+        Uri.https('flutter747a-default-rtdb.firebaseio.com', '/products.json');
+    http
+        .post(
+      url,
+      body: json.encode({
+        'title': product.title,
+        'description': product.description,
+        'price': product.price,
+        'imageUrl': product.imageUrl,
+        'isFavorite': product.isFavorite,
+      }),
+    )
+        .then((response) {
+      final newProduct = Product(
+        title: product.title,
+        description: product.description,
+        price: product.price,
+        imageUrl: product.imageUrl,
+        id: json.decode(response.body)['name'],
+      );
+      // _items.add(newProduct);
+      _items.insert(0, newProduct);
+      notifyListeners();
+    });
     // _items.add(value);
-    final newProduct = Product(
-      title: product.title,
-      description: product.description,
-      price: product.price,
-      imageUrl: product.imageUrl,
-      id: DateTime.now().toString(),
-    );
-    // _items.add(newProduct);
-    _items.insert(0, newProduct);
-    notifyListeners();
   }
 
   void updateProduct(String id, Product newProduct) {
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -90,6 +90,8 @@ class _EditProductScreenState extends State<EditProductScreen> {
       Provider.of<Products>(context, listen: false)
           .updateProduct(_editedProduct.id, _editedProduct);
     } else {
+
+      
       Provider.of<Products>(context, listen: false).addProduct(_editedProduct);
     }
     Navigator.of(context).pop();
